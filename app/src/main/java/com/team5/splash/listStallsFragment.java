381533460bd62ee3bd176ab4582e83a6d5e4de59
awhitepen <76237481+awhitepen@users.noreply.github.com>
2021-08-06 16:32:12 +0800

    Fragment open fragment works :) Test in the Hawkers Fragment

--- app/src/main/java/com/team5/splash/listStallsFragment.java
+++ app/src/main/java/com/team5/splash/listStallsFragment.java
@@ -8,18 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link hawkerFragment#newInstance} factory method to
+ * Use the {@link listStallsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class hawkerFragment extends Fragment {
+public class listStallsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,9 +25,7 @@ public class hawkerFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public int getId;
-
-    public hawkerFragment() {
+    public listStallsFragment() {
         // Required empty public constructor
     }
 
@@ -42,11 +35,11 @@ public class hawkerFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment hawkerFragment.
+     * @return A new instance of fragment listStallsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static hawkerFragment newInstance(String param1, String param2) {
-        hawkerFragment fragment = new hawkerFragment();
+    public static listStallsFragment newInstance(String param1, String param2) {
+        listStallsFragment fragment = new listStallsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,35 +52,24 @@ public class hawkerFragment extends Fragment {
         super.onStart();
 
         View view = getView();
-        AppCompatButton findStallsBtn = view.findViewById(R.id.findStallsBtn);
+        AppCompatButton stallDetailsBtn = view.findViewById(R.id.stallDetailsBtn);
 
-        findStallsBtn.setOnClickListener(new View.OnClickListener() {
+        stallDetailsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Toast.makeText(getContext(), "Hello I've been clicked!", Toast.LENGTH_SHORT).show();
-
-//                if (fragmentContainer != null) {
-                    // Add the fragment to the FrameLayout
-                    replaceFragment();
-//                }
-
+                replaceFragment();
             }
         });
+
     }
 
     public void replaceFragment()
     {
-        Fragment fragment = new listStallsFragment();
+        Fragment fragment = new stallFragment();
 
         this.getParentFragmentManager().beginTransaction()
                 .replace(((ViewGroup) getView().getParent()).getId(), fragment).addToBackStack(null).commit();
 
-//        FragmentManager fm = getActivity().getSupportFragmentManager();
-//        FragmentTransaction trans = fm.beginTransaction();
-//        trans.hide(this).show(fragment);
-//        trans.replace(getId, fragment);
-//        trans.addToBackStack(null);
-//        trans.commit();
     }
 
     @Override
@@ -102,8 +84,7 @@ public class hawkerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hawker, container, false);
+        return inflater.inflate(R.layout.fragment_list_stalls, container, false);
     }
 }
