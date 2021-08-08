4ea2785a836ec56fb6e5a2fc799335a1a5d30cd2
awhitepen <76237481+awhitepen@users.noreply.github.com>
2021-08-08 09:08:36 +0800

    - Data from 5000/api/listCentre now parsed as JSON Array successfully
    Next step:
    - put data into ListView via ArrayAdaptor

--- app/src/main/java/com/team5/splash/hawkerFragment.java
+++ app/src/main/java/com/team5/splash/hawkerFragment.java
@@ -15,8 +15,13 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 /**
@@ -89,11 +94,21 @@ public class hawkerFragment extends Fragment {
     {
         String url = "http://192.168.1.177:5000/api/listCentre";
 
-        StringRequest request = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(String response) {
-                        listHawkerCentres.setText(response);
+                    public void onResponse(JSONArray response) {
+                        for (int i=0; i < response.length(); i++)
+                        {
+                            try {
+                                JSONObject hawkerCentre = response.getJSONObject(i);
+
+                                listHawkerCentres.append(hawkerCentre.getString("name") + " " + hawkerCentre.getString("address") + "\n\n");
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
