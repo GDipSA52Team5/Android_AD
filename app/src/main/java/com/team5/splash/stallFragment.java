3f43764cdbada0205508470c0b12e932aa094670
awhitepen <76237481+awhitepen@users.noreply.github.com>
2021-08-11 19:11:53 +0800

    Open Map with Pin

--- app/src/main/java/com/team5/splash/stallFragment.java
+++ app/src/main/java/com/team5/splash/stallFragment.java
@@ -1,5 +1,7 @@
 package com.team5.splash;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -95,7 +97,15 @@ public class stallFragment extends Fragment {
         OpenMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity().getApplicationContext(), "Hello you want directions?", Toast.LENGTH_SHORT).show();
+
+                String latLongDirections = "geo:" + hc.getLatitude() + "," + hc.getLongitude() + "q=" + hc.getLatitude() + "," + hc.getLongitude() + "(" + hc.getName() + ")";
+                Uri uri = Uri.parse(latLongDirections);
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+                startActivity(intent);
+
+
             }
         });
 
