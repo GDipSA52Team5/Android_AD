baf24442e39a881841b97996ea889f1ce3eedefd
Hexuesheng-666 <1413778858@qq.com>
2021-08-20 16:40:06 +0800

    change api and make favourite button invisible

---
+++ app/src/main/java/com/team5/splash/Favourite.java
@@ -0,0 +1,20 @@
+package com.team5.splash;
+
+public class Favourite {
+
+    private int favouriteStall;
+
+    public int getFavouriteStall() {
+        return favouriteStall;
+    }
+
+    public Favourite() {
+    }
+
+    public void setFavouriteStall(int favouriteStall) {
+        this.favouriteStall = favouriteStall;
+    }
+
+
+
+}
