1b6637a7a445bf08032edf2d4ece9c769e7eec53
w-zerita <79044966+w-zerita@users.noreply.github.com>
2021-08-20 10:52:57 +0800

    individual menu item commit2

--- app/src/main/java/com/team5/splash/menuItemFragment.java
+++ app/src/main/java/com/team5/splash/menuItemFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
