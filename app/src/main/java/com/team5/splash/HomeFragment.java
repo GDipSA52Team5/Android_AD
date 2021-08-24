4cb63ee245a902c1b3de4d912021300128a627bf
awhitepen <76237481+awhitepen@users.noreply.github.com>
2021-08-24 09:19:31 +0800

    Update Home Fragment for Guest - lacking onClickListener

--- app/src/main/java/com/team5/splash/HomeFragment.java
+++ app/src/main/java/com/team5/splash/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.team5.splash;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +34,14 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+
+    private Context mContext;
+    private View view;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private String userDisplayName;
+    private List<HawkerStall> hawkerStalls = new ArrayList<HawkerStall>();
+    private TextView homeTxt;
+    private ListView listHawkerStalls;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -52,6 +80,97 @@ public class HomeFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        mContext = getContext();
+        view = getView();
+
+        homeTxt = view.findViewById(R.id.homeTxt);
+        listHawkerStalls = view.findViewById(R.id.listReccomended);
+
+        if (user == null)
+        {
+            homeTxt.setText(getString(R.string.welcome_guest));
+            getTopStalls();
+        }
+        else
+        {
+            userDisplayName = user.getDisplayName();
+
+            homeTxt.setText("Hello " + userDisplayName + getString(R.string.welcome_user));
+        }
+
+    }
+
+    public void getTopStalls()
+    {
+        String url = "https://gdipsa-ad-ml.herokuapp.com/highestRatedStalls";
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for (int i=0; i < response.length(); i++)
+                        {
+                            try {
+                                JSONObject hawkerStallObj = response.getJSONObject(i);
+
+                                HawkerStall hawkerStall = new HawkerStall();
+
+                                hawkerStall.setId(hawkerStallObj.getInt("id"));
+                                hawkerStall.setStallName(hawkerStallObj.getString("stall_name"));
+                                hawkerStall.setUnitNumber(hawkerStallObj.getString("unit_number"));
+                                hawkerStall.setContactNumber(hawkerStallObj.getString("contact_number"));
+                                hawkerStall.setStatus(hawkerStallObj.getString("status"));
+                                hawkerStall.setOperatingHours(hawkerStallObj.getString("operating_hours"));
+                                hawkerStall.setStallImgUrl(hawkerStallObj.getString("hawker_img"));
+
+                                hawkerStalls.add(hawkerStall);
+
+                                if(i == (response.length() - 1))
+                                {
+                                    createListStallsView();
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(mContext, "Error Retrieving Top Stalls", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MySingleton.getInstance(mContext).addToRequestQueue(request);
+    }
+
+    public void createListStallsView()
+    {
+
+        ListHawkerStallsAdaptor adaptor = new ListHawkerStallsAdaptor(mContext, hawkerStalls);
+
+        if(listHawkerStalls !=null)
+        {
+            listHawkerStalls.setAdapter(adaptor);
+
+//             implement onItemClick(...) for listView
+            listHawkerStalls.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    if (hawkerStalls.size() != 0)
+                    {
+                        // replace fragment
+                    }
+                }
+            });
         }
     }
 
