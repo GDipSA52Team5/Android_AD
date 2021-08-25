4893bc7a2844ecde9f02e3b8fe63c9a31b4cd114
GrimPerspective <45945593+GrimPerspective@users.noreply.github.com>
2021-08-25 16:31:25 +0800

    25 Aug - Commit 13 - Bug fix - menu item (Back)

--- app/src/main/java/com/team5/HawkeRise/fragments/Hawkers_StallInfoFragment.java
+++ app/src/main/java/com/team5/HawkeRise/fragments/Hawkers_StallInfoFragment.java
@@ -425,6 +425,7 @@ public class Hawkers_StallInfoFragment extends Fragment {
         Fragment fragment = new Hawkers_MenuFragment();
         fragment.setArguments(arguments);
 
+        menuItems.removeAll(menuItems);
         this.getParentFragmentManager().beginTransaction()
                 .replace(((ViewGroup) getView().getParent()).getId(), fragment, null)
                 .addToBackStack(null)
